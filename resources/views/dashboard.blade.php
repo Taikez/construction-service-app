61ede92e7610cc462594a35088c8e4a9510567f0
Taikez <juliannardita@gmail.com>
2024-02-23 22:40:57 +0700

    finished banner, offers and whats app API

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,15 +5,50 @@
 @endsection
 
 @section('main')
-    <div id="banner" class="container-fluid d-flex align-items-center">
+    <div id="banner" class="container-fluid d-flex align-items-center pb-5">
         <div id="banner-text-container" class="col-lg-8 col-md-8 col-sm-12">
             <div id="banner-text" class="container col-lg-6 col-md-6 col-sm-12 p-5 rounded" data-aos="fade-up" data-aos-duration="1500">
-                <div data-aos="fade" data-aos-duration="3000">
-                    <h1 class="fw-bold col-lg-8 col-md-8 col-sm-12">Suheri's Construction Services</h1>
+                <div data-aos="fade" data-aos-duration="1500" data-aos-delay="1000">
+                    <div class="row">
+                        <h1 class="fw-bold col-lg-8 col-md-8 col-sm-12">Suheri's Construction Services</h1>
+                    </div>
                     <hr class="divider">
-                    <p class="mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis voluptatibus aliquid error ex modi suscipit incidunt voluptates iusto. Ullam suscipit, dicta debitis cumque accusantium assumenda aspernatur voluptas harum ad iusto.</p>
-                    <button id="contact-btn" class="btn btn-lg text-white px-5 fs-6">HUBUNGI KAMI</button>
+                    <p class="mb-5 lh-lg">
+                        Selamat datang di <b><i>Suheri's Construction Services</i></b>! Kami adalah mitra <b>terpercaya</b> untuk proyek konstruksi Anda, memberikan layanan <b>berkualitas tinggi</b> yang sesuai dengan kebutuhan dan harapan Anda. Terima kasih atas kepercayaannya!</p>
+                    <div class="row">
+                        <a id="contact-btn" class="btn btn-lg text-white fs-6 col-lg-4 col-md-4 col-sm-12" href="https://api.whatsapp.com/send?phone=6285811568097&amp;text=Halo%20Mas%20Suheri%20%2C%20Mohon%20Info%20Pasang%20dan%20Renovasi%20Atap%20Baja%20Ringan">
+                            <div id="contact-btn-text" class="d-flex justify-content-between align-items-center">
+                                HUBUNGI KAMI
+                                <i class="fa-solid fa-phone"></i>
+                            </div>
+                        </a>
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div id="our-offers" class="pt-5">
+        <h1 class="fw-bold text-center text-white pt-5 pb-5" data-aos="fade" data-aos-duration="3000">Penawaran Kami</h1>
+        <div id="offers" class="row d-flex justify-content-center align-items-center p-5 me-5 lh-lg">
+            <div id="offer-container" class="col-lg-3 col-md-12 col-sm-12 text-center px-5 mb-5" data-aos="fade-up-right"
+                data-aos-duration="1500">
+                <i id="offer-icon" class="fa-solid fa-thumbs-up fs-1 mb-5"></i>
+                <h3 class="fw-bold text-white">Kuat Dan Rapih</h3>
+                <p class="d-flex justify-content-center text-white">Layanan konstruksi berkualitas, kuat, dan rapih, sesuai harapan Anda.</p>
+            </div>
+            <div id="offer-container" class="col-lg-3 col-md-12 col-sm-12 text-center px-5 mb-5" data-aos="fade-up"
+                data-aos-duration="1500" data-aos-delay="200">
+                <i id="offer-icon" class="fa-solid fa-bolt fs-1 mb-5"></i>
+                <h3 class="fw-bold text-white">Cepat dan Tepat</h3>
+                <p class="d-flex justify-content-center text-white">
+                    Layanan konstruksi yang cepat, tetapi tidak lupa dengan ketepatan.</p>
+            </div>
+            <div id="offer-container" class="col-lg-3 col-md-12 col-sm-12 text-center px-5 mb-5" data-aos="fade-up-left"
+                data-aos-duration="1500" data-aos-delay="200">
+                <i id="offer-icon" class="fa-solid fa-arrow-right-arrow-left fs-1 mb-5"></i>
+                <h3 class="fw-bold text-white">Fleksibel</h3>
+                <p class="d-flex justify-content-center text-white">
+                    Layanan konstruksi fleksibel, jasa disesuaikan dengan lingkungan yang dihadapi.</p>
             </div>
         </div>
     </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,12 @@
 @vite(['resources/css/header.css'])
 
-<nav class="navbar navbar-expand-lg p-4">
+<nav class="navbar navbar-expand-lg p-4 sticky-top">
     <div class="container-fluid" data-aos="fade" data-aos-duration="1500">
         <a class="navbar-brand col-8 text-light fw-bold d-flex" href="{{ route('dashboard') }}">
             <i class="fa-solid fa-person-digging fs-1"></i>
         </a>
-            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon bg-light"></span>
+            <button class="navbar-toggler bg-white border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon bg-white text-white"></span>
             </button>
         <div class="collapse navbar-collapse col-4" id="navbarNav">
             <ul class="navbar-nav h-100">
